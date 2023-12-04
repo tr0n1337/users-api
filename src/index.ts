f1014c8ad9d90e5d6546af7125a19e2e5615bb8e
Thiago Fernandes <thiago.souzax3@outlook.com>
2023-12-04 01:22:33 -0300

    feat: add route post create user

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import * as express from "express";
 import { Request, Response } from "express";
 import { config } from "dotenv";
-import { GetUsersController } from "@/controllers/get-users/get-users";
-import { MongoGetUsersRepository } from "@/repositories/get-users/mongo-get-users";
 import { MongoClient } from "@/database/mongo";
+import { GetUsersController } from "@/controllers/get-users/get-users";
+import { CreateUserController } from "@/controllers/create-user/create-user";
+import { MongoGetUsersRepository } from "@/repositories/get-users/mongo-get-users";
+import { MongoCreateUserRepository } from "@/repositories/create-user/mongo-create-user";
 
 const main = async () => {
   config();
 
   const app = express();
+
+  app.use(express.json());
 
   await MongoClient.connect();
 
@@ -19,7 +23,21 @@ const main = async () => {
 
     const { body, statusCode } = await getUsersController.handle();
 
-    res.send(body).status(statusCode);
+    res.status(statusCode).send(body);
+  });
+
+  app.post("/users", async (req: Request, res: Response) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository,
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
+
+    res.status(statusCode).send(body);
   });
 
   const port = process.env.PORT || 8000;
