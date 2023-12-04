98c3d9fa01a07ac6f8a46b52b5429a9052ea4619
Thiago Fernandes <thiago.souzax3@outlook.com>
2023-12-03 23:19:59 -0300

    feat: ensure GetUsers gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,22 @@
 import { IGetUsersRepository } from "@/controllers/get-users/protocols";
+import { MongoClient } from "@/database/mongo";
 import { User } from "@/models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Thiago",
-        lastName: "Fernandes",
-        email: "thiago.souzax3@outlook.com",
-        password: "123456",
-      },
-    ];
+    try {
+      const users = await MongoClient.db
+        .collection<Omit<User, "id">>("users")
+        .find({})
+        .toArray();
+
+      return users.map(({ _id, ...rest }) => ({
+        ...rest,
+        id: _id.toHexString(),
+      }));
+    } catch (error) {
+      console.error("Erro ao obter usuários:", error);
+      throw error;
+    }
   }
 }
